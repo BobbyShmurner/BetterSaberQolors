02dd123312413210ece9d28d06979e02ddcd4db2
Ohdeymus <glitched.ohdeymus@gmail.com>
2021-10-18 20:09:26 +0100

    Fixed some version issues with the qmod build script

--- buildQMOD.ps1
+++ buildQMOD.ps1
@@ -6,9 +6,9 @@ if (-not ($PSVersionTable.PSEdition -eq "Core")) {
     $buildScript += ".cmd"
 }
 
-$ArchiveName = "BetterSaberQolors_v0.1.0.qmod"
-$TempArchiveName = "BetterSaberQolors_v0.1.0.qmod.zip"
+$ArchiveName = "BetterSaberQolors_v1.0.0.qmod"
+$TempArchiveName = "BetterSaberQolors_v1.0.0.qmod.zip"
 
 & $buildScript NDK_PROJECT_PATH=$PSScriptRoot APP_BUILD_SCRIPT=$PSScriptRoot/Android.mk NDK_APPLICATION_MK=$PSScriptRoot/Application.mk
-Compress-Archive -Path "./libs/arm64-v8a/libBetterSaberQolors.so", "./libs/arm64-v8a/libbeatsaber-hook_2_3_0.so", "./cover.png", "./mod.json" -DestinationPath $TempArchiveName -Force
+Compress-Archive -Path "./libs/arm64-v8a/libBetterSaberQolors.so", "./libs/arm64-v8a/libbeatsaber-hook_2_3_2.so", "./cover.png", "./mod.json" -DestinationPath $TempArchiveName -Force
 Move-Item $TempArchiveName $ArchiveName -Force
